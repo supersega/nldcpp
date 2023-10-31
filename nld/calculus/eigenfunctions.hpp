8be4ff741a3be8999b3fe0e630494a6c3786d49b
supersega <malyshev13091995@gmail.com>
2023-10-31 18:42:46 +0200

    fix type for count in eigenfunctions

--- nld/calculus/eigenfunctions.hpp
+++ nld/calculus/eigenfunctions.hpp
@@ -4,21 +4,22 @@
 
 namespace nld {
 /// @brief The eigenfunctions of differential operator.
-template<typename T>
+template <typename T>
 struct eigenfunctions final {
     static constexpr nld::index dimension = 1;
 
     /// @brief Construct a new eigenfunctions object.
     /// @param test_functions The test functions.
     /// @param eigenvectors The eigenvectors.
-    explicit eigenfunctions(T test_functions, nld::matrix_xd&& eigenvectors, nld::index size) : 
-        test_functions(test_functions), eigenvectors(std::move(eigenvectors)), size(size) {
+    explicit eigenfunctions(T test_functions, nld::matrix_xd &&eigenvectors,
+                            nld::index size)
+        : test_functions(test_functions), eigenvectors(std::move(eigenvectors)),
+          size(size) {}
 
-    }
-
-    /// @brief The value compute function of i-th test function 
+    /// @brief The value compute function of i-th test function
     /// @param i index of test function
-    /// @return Function f(x) -> std::array<double, Dim> to compute i-th function at point x.
+    /// @return Function f(x) -> std::array<double, Dim> to compute i-th
+    /// function at point x.
     auto value(nld::index i) const {
         return [this, i](auto x) -> adnum {
             auto eigenvector = this->eigenvectors.col(i);
@@ -34,25 +35,22 @@ struct eigenfunctions final {
     /// @brief The value compute function of i-th eigenfunction.
     /// @param i index of eigenfunction.
     /// @return Function f(x) -> adnum to compute i-th eigenfunction at point.
-    auto value(std::tuple<nld::index> i) const {
-        return value(std::get<0>(i));
-    }
+    auto value(std::tuple<nld::index> i) const { return value(std::get<0>(i)); }
 
     /// @brief Count of eigenfunctions for approximation.
     /// @returns Number of eigenfunctions.
-    auto count() const -> nld::index {
-        return size;
-    }
+    auto count() const -> std::tuple<nld::index> { return std::tuple(size); }
 
     /// @brief Get the space object.
     /// @return Space where test functions are defined.
-    auto get_space() const -> const auto& {
+    auto get_space() const -> const auto & {
         return test_functions.get_space();
     }
+
 private:
     T test_functions;            ///< The test functions.
     nld::matrix_xd eigenvectors; ///< The eigenvectors.
     nld::index size;             ///< The count of eigenfunctions to analyze.
 };
- 
-}
+
+} // namespace nld
--- nld/calculus/eval.hpp
+++ nld/calculus/eval.hpp
@@ -6,57 +6,81 @@
 #include <nld/calculus/tensor_size.hpp>
 
 namespace nld::internal {
-template<typename I, typename E, typename D>
-auto eval_impl(const E& mul, const D& domain, integration_options options) requires (tensor_size<E>::size == 1) {
+template <typename I, typename E, typename D>
+auto eval_impl(const E &mul, const D &domain, integration_options options)
+    requires(tensor_size<E>::size == 1)
+{
     auto [ni] = mul.count();
     nld::tensor<1> result(ni);
     for (nld::index i = 0; i < ni; i++)
-        result(i) = nld::integrate<I>([i, &mul](auto... x) { return static_cast<double>(mul.value(std::tuple(i))(x...)); }, domain, options);
-    
+        result(i) = nld::integrate<I>(
+            [i, &mul](auto... x) {
+                return static_cast<double>(mul.value(std::tuple(i))(x...));
+            },
+            domain, options);
+
     return result;
 }
 
-template<typename I, typename E, typename F, typename D>
-auto eval_impl(const nld::dirac_shift<E, F>& shift, const D& domain, [[maybe_unused]] integration_options options) {
-    auto ni = shift.expression.count();
+template <typename I, typename E, typename F, typename D>
+auto eval_impl(const nld::dirac_shift<E, F> &shift, const D &domain,
+               [[maybe_unused]] integration_options options) {
+    auto [ni] = shift.expression.count();
     nld::tensor<1> result(ni);
     for (nld::index i = 0; i < ni; i++)
-        result(i) = static_cast<double>(std::apply(shift.expression.value(std::tuple(i)), shift.delta_function.coords()));
-    
+        result(i) = static_cast<double>(
+            std::apply(shift.expression.value(std::tuple(i)),
+                       shift.delta_function.coords()));
+
     return result;
 }
 
-template<typename I, typename E, typename D>
-auto eval_impl(const E& mul, const D& domain, integration_options options) requires (tensor_size<E>::size == 2) {
+template <typename I, typename E, typename D>
+auto eval_impl(const E &mul, const D &domain, integration_options options)
+    requires(tensor_size<E>::size == 2)
+{
     auto [ni, nj] = mul.count();
     nld::tensor<2> result(nj, ni);
     for (nld::index i = 0; i < ni; i++)
         for (nld::index j = 0; j < nj; j++) {
             if constexpr (SubdomainDefined<E>) {
                 auto subdomain = mul.subdomain(std::tuple(j, i));
-                subdomain = subdomain ? domain.intersect(*subdomain) : std::nullopt;
-                result(j, i) = subdomain ? nld::integrate<I>([j, i, &mul](auto... x) { return static_cast<double>(mul.value(std::tuple(j, i))(x...)); }, *subdomain, options) : 0.0;
-            }
-            else {
-                result(j, i) = nld::integrate<I>([j, i, &mul](auto... x) { return static_cast<double>(mul.value(std::tuple(j, i))(x...)); }, domain, options);
+                subdomain =
+                    subdomain ? domain.intersect(*subdomain) : std::nullopt;
+                result(j, i) =
+                    subdomain ? nld::integrate<I>(
+                                    [j, i, &mul](auto... x) {
+                                        return static_cast<double>(
+                                            mul.value(std::tuple(j, i))(x...));
+                                    },
+                                    *subdomain, options)
+                              : 0.0;
+            } else {
+                result(j, i) = nld::integrate<I>(
+                    [j, i, &mul](auto... x) {
+                        return static_cast<double>(
+                            mul.value(std::tuple(j, i))(x...));
+                    },
+                    domain, options);
             }
         }
     return result;
 }
 
-template<typename I, typename E, typename D>
-auto eval_impl(const E& expr, const D& domain, integration_options options) requires (tensor_size<E>::size == 3) {
+template <typename I, typename E, typename D>
+auto eval_impl(const E &expr, const D &domain, integration_options options)
+    requires(tensor_size<E>::size == 3)
+{}
 
-}
+template <typename I, typename E, typename D>
+auto eval_impl(const E &expr, const D &domain, integration_options options)
+    requires(tensor_size<E>::size == 4)
+{}
 
-template<typename I, typename E, typename D>
-auto eval_impl(const E& expr, const D& domain, integration_options options) requires (tensor_size<E>::size == 4) {
-
-}
-
-template<typename I, typename E, typename D>
-auto eval(const E& expr, const D& domain, integration_options options) -> nld::tensor<tensor_size<E>::size> {
+template <typename I, typename E, typename D>
+auto eval(const E &expr, const D &domain, integration_options options)
+    -> nld::tensor<tensor_size<E>::size> {
     return eval_impl<I>(expr, domain, options);
 }
 
-}
+} // namespace nld::internal
