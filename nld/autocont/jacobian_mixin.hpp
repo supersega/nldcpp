667692a744880ba1b9d049094e6847fe307d2bf0
supersega <malyshev13091995@gmail.com>
2023-12-02 22:24:30 +0200

    simplify jacobian mixin

--- nld/autocont/jacobian_mixin.hpp
+++ nld/autocont/jacobian_mixin.hpp
@@ -13,30 +13,22 @@ namespace nld {
 /// TODO: Consider allocators for Eigen vectors and matrices.
 /// Now we have a lot memory allocation on heap.
 
-/// @brief Class to inject jacobian functions for nonlinear functions using operator().
+/// @brief Class to inject jacobian functions for nonlinear functions using
+/// operator().
 /// @details We use boundary_value_problem in this class, and it can use any
 /// integration method. But we use Runge-Kutta 4th order now.
 /// @tparam F Function.
 /// @tparam P Evaluation parameters.
-template<typename F>
+template <typename F>
 struct jacobian_mixin : nld::utils::crtp<F> {
     /// @brief Jacobian of dynamic system.
     /// @param wrt Variables w.r.t. we evaluate jacobian.
     /// @param at Point where we evaluate jacobian.
     /// @param v Value of function if given point.
     /// @return Jacobi matrix.
-    template<typename Wrt, typename At, typename Result>
-    auto jacobian(Wrt&& wrt, At&& at, Result& v) const {
+    template <typename Wrt, typename At, typename Result>
+    auto jacobian(Wrt &&wrt, At &&at, Result &v) const {
         return autodiff::forward::jacobian(this->derived(), wrt, at, v);
     }
-
-    /// @brief Jacobian of dynamic system.
-    /// @param wrt Variables w.r.t. we evaluate jacobian.
-    /// @param at Point where we evaluate jacobian.
-    /// @return Jacobi matrix.
-    template<typename Wrt, typename At>
-    auto jacobian(Wrt&& wrt, At&& at) const {
-        return autodiff::forward::jacobian(this->derived(), wrt, at);
-    }
 };
-} // end namespace nld::wrappers
+} // namespace nld
--- nld/autocont/mappers.hpp
+++ nld/autocont/mappers.hpp
@@ -9,9 +9,10 @@ namespace nld {
 /// @brief Concatenate map functions.
 /// @param map map functions.
 /// @returns function which creates tuple of maps results.
-template<typename... M>
-constexpr auto concat(M&&... map) noexcept {
-    return [... map = std::forward<M>(map)](const auto& fn, const auto& v) noexcept {
+template <typename... M>
+constexpr auto concat(M &&...map) noexcept {
+    return [... map = std::forward<M>(map)](const auto &fn,
+                                            const auto &v) noexcept {
         return std::tuple(map(fn, v)...);
     };
 };
@@ -19,7 +20,7 @@ constexpr auto concat(M&&... map) noexcept {
 /// @brief Function to create map which returns variables.
 /// @returns solution of nonlinear system.
 constexpr auto solution() {
-    return [](const auto& fn, const auto& v) noexcept { return v; };
+    return [](const auto &fn, const auto &v) noexcept { return v; };
 }
 
 /// @brief Function to create 2d point from solution.
@@ -27,22 +28,22 @@ constexpr auto solution() {
 /// @param m second index of variable from solution.
 /// @returns tuple(v[N], r)
 constexpr auto point2d(nld::index n, nld::index m) noexcept {
-    return [n, m](const auto& fn, const auto& v) noexcept { return std::tuple(v[n], v[m]); };
+    return [n, m](const auto &fn, const auto &v) noexcept {
+        return std::tuple(v[n], v[m]);
+    };
 }
 
 /// @brief Function to request mean amplitude from periodic problem.
 /// @param coordinate index of coordinate.
 /// @returns mean amplitude of coordinate A.
 constexpr auto mean_amplitude(nld::index coordinate) noexcept {
-    return [coordinate]<typename P>
-    (const P& periodic, const auto& variables) noexcept {
+    return [coordinate]<typename P>(const P &periodic,
+                                    const auto &variables) noexcept {
         auto [initial, args] = integration_arguments(variables, periodic);
 
-        return mean<typename P::solver_t>(
-            periodic.underlying_function(),
-            periodic.integration_parameters(),
-            initial,
-            args)[coordinate];
+        return mean<typename P::solver_t>(periodic.underlying_function(),
+                                          periodic.integration_parameters(),
+                                          initial, args)[coordinate];
     };
 }
 
@@ -50,23 +51,26 @@ constexpr auto mean_amplitude(nld::index coordinate) noexcept {
 /// @param coordinate index of coordinate.
 /// @returns mean amplitude of coordinate A.
 constexpr auto half_swing(nld::index coordinate) noexcept {
-    return [coordinate]<typename P>
-    (const P& periodic, const auto& variables) noexcept {
-        return std::tuple(variables(variables.size() - 1), mean_amplitude(coordinate)(periodic, variables));
+    return [coordinate]<typename P>(const P &periodic,
+                                    const auto &variables) noexcept {
+        return std::tuple(variables(variables.size() - 1),
+                          mean_amplitude(coordinate)(periodic, variables));
     };
 }
 
 /// @brief Mapper what evaluates monodromy matrix for periodic .
 /// @returns Lambda object what evaluates monodromy Matrix.
 constexpr auto monodromy() noexcept {
-    return []<typename P>
-    (const P& periodic, auto variables) noexcept {
-        const auto dimension = variables.size() - periodic.non_state_variables();
+    return []<typename P>(const P &periodic, auto variables) noexcept {
+        const auto dimension =
+            variables.size() - periodic.non_state_variables();
 
-        auto jacobian = periodic.jacobian(wrt(variables.head(dimension)), at(variables));
+        decltype(periodic(variables)) result(variables.size());
+        auto jacobian = periodic.jacobian(wrt(variables.head(dimension)),
+                                          at(variables), result);
         jacobian += nld::matrix_xd::Identity(dimension, dimension);
 
-        return jacobian; 
+        return jacobian;
     };
 }
-}
+} // namespace nld
